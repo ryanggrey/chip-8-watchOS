5d19a6ac82ea3821f745d9bcf10e79ed8ade370f
Ryan Grey <ryan.g.grey@gmail.com>
2021-02-09 22:54:39 +0000

    Add stub project

---
+++ CHIP8WatchOS WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  CHIP8WatchOS WatchKit Extension
+//
+//  Created by Ryan Grey on 09/02/2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
